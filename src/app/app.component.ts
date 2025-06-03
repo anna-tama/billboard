e25f5fd8945ea90f67a3f681e3c55e54169666be
Tamara Romanczuk <112263180+anna-tama@users.noreply.github.com>
2025-06-02 21:09:52 -0300

    main: firebase ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ReactiveFormsModule],
+  imports: [RouterOutlet,ReactiveFormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyDbjsY-QYWMetSPLLaZ0jq65kSzcGxkPUQ",
+        authDomain: "billboard-65503.firebaseapp.com",
+        projectId: "billboard-65503",
+        storageBucket: "billboard-65503.firebasestorage.app",
+        messagingSenderId: "933096174810",
+        appId: "1:933096174810:web:feea7004e7ed2380d7a1e3"
+    }
+};
